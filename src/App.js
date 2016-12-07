0024190e0c86b3dc66643b25f593e8612ec5d467
Fayimora Femi-Balogun <fayi@fayimora.com>
2016-12-07 00:08:12 +0000

    propTypes and defaultProps :wink:

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
+    const name = this.props.name;
     return (
       <div>
         <h1>Hello world</h1>
         <p>Welcome to react...</p>
+        <p>{name}, react seems cool...</p>
       </div>
     )
   }
+}
+
+App.propTypes = {
+  name: PropTypes.string.isRequired
+}
+
+App.defaultProps = {
+  name: "Anastasiya"  
 }
 
 // const App = () => <h1>Hello word from stateless!!!</h1>
