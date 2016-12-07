a0df8cdbedd3b903368091c12de697a66dd8d2a9
Fayimora Femi-Balogun <fayi@fayimora.com>
2016-12-07 00:42:10 +0000

    Give me some eventssssss

--- src/App.js
+++ src/App.js
@@ -5,23 +5,41 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      age: 22
+      age: 22,
+      currentEvent: '----'
     }
+    this.update = this.update.bind(this)
   }
 
   update(e) {
     this.setState({age: e.target.value})
+    this.setState({currentEvent: e.type})
   }
 
   render() {
     const name = this.props.name;
     return (
       <div>
-        <Greeting>Hello world <Heart /></Greeting>
-        <p>Welcome to react...</p>
-        <p>{name}, react seems cool...</p>
-        <Widget update={this.update.bind(this)}></Widget>
-        <p>I am {this.state.age} years old</p>
+        <div>
+          <Greeting>Hello world <Heart /></Greeting>
+          <p>Welcome to react...</p>
+          <p>{name}, react seems cool...</p>
+          <Widget update={this.update}></Widget>
+          <p>I am {this.state.age} years old</p>
+        </div>
+
+        <hr/>
+
+        <div>
+          <textarea
+            cols="30"
+            rows="10"
+            onCopy={this.update}
+            onFocus={this.update}
+            onBlur={this.update}
+            onKeyPress={this.update}/>
+          <h1>{this.state.currentEvent}</h1>
+        </div>
       </div>
     )
   }
